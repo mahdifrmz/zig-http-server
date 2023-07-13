83d548d9d0a771af706aa6c216ec4299d183aaa3
mahdifrmz <faramarzpour98@gmail.com>
2023-07-13 17:37:36 +0330

    feat: run TCP server

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,29 @@
 const std = @import("std");
 const allocator = std.heap.page_allocator;
+const ArrayList = std.ArrayList;
+const ArgList = ArrayList([]const u8);
 
-pub fn main() !void {
+fn getArgs() !ArgList {
+    var arglist = ArgList.init(allocator);
     var argIter = try std.process.argsWithAllocator(allocator);
     while (true) {
         const arg = argIter.next() orelse break;
-        std.debug.print("-> {s}\n", .{arg});
+        try arglist.append(arg);
     }
     argIter.deinit();
+    return arglist;
+}
+
+pub fn main() !void {
+    const args = try getArgs();
+    const port: u16 = if (args.items.len < 2)
+        8080
+    else
+        try std.fmt.parseInt(u16, args.items[1], 10);
+    var server = std.net.StreamServer.init(.{});
+    try server.listen(try std.net.Address.resolveIp("0.0.0.0", port));
+    while (true) {
+        const client = try server.accept();
+        _ = client;
+    }
 }
