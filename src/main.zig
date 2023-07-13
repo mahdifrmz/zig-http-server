aefc487b7c4034b259d8bf0ce6d114d903419c22
mahdifrmz <faramarzpour98@gmail.com>
2023-07-13 18:02:16 +0330

    feat: logging

--- src/main.zig
+++ src/main.zig
@@ -14,14 +14,28 @@ fn getArgs() !ArgList {
     return arglist;
 }
 
+fn print(comptime fmt: []const u8, args: anytype) void {
+    const writer = std.io.getStdOut().writer();
+    std.fmt.format(writer, fmt, args) catch {};
+}
+fn eprint(comptime fmt: []const u8, args: anytype) void {
+    const writer = std.io.getStdErr().writer();
+    std.fmt.format(writer, fmt, args) catch {};
+}
+
 pub fn main() !void {
     const args = try getArgs();
     const port: u16 = if (args.items.len < 2)
         8080
     else
-        try std.fmt.parseInt(u16, args.items[1], 10);
+        std.fmt.parseInt(u16, args.items[1], 10) catch {
+            eprint("Error: Invalid port number", .{});
+            std.process.exit(1);
+        };
     var server = std.net.StreamServer.init(.{});
     try server.listen(try std.net.Address.resolveIp("0.0.0.0", port));
+    print("Zerver listening on port {d}", .{port});
+
     while (true) {
         const client = try server.accept();
         _ = client;
